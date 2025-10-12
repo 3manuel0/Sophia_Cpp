0a709b0434a7ec3b48152b96e09eeb0b98b73820
3manuel0 <saiddimension@gmail.com>
2025-10-12 01:05:01 +0100

    added the number game

--- main.cpp
+++ main.cpp
@@ -1,45 +1,135 @@
+#include "details.hpp"
 #include <algorithm>
+#include <cctype>
 #include <dpp/dpp.h>
+#include <random>
 #include <string>
 
-int main() {
-  const char *BOT_TOKEN = std::getenv("BOT_TOKEN");
-  if (!BOT_TOKEN) {
-    std::cerr << "BOT_TOKEN missing from .env file!\n";
-    return 1;
+bool isNumber(const std::string &s) {
+  if (s.empty())
+    return false;
+
+  for (char c : s) {
+    if (c < '0' || c > '9') {
+      return false;
+    }
   }
+
+  return true;
+}
+
+bool isNotRepeated(std::string &s) {
+  if (s.empty())
+    return false;
+
+  int i = 0;
+  int j = 1;
+
+  while (i < 4) {
+    while (j < 4) {
+      if (s[i] == s[j])
+        return false;
+      j++;
+    }
+    i++;
+    j = i + 1;
+  }
+
+  return true;
+}
+
+std::string strAnswer(std::string &reply, std::string &s) {
+  char e = 0;
+  char p = 0;
+
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+
+      if (reply[j] == s[i]) {
+        if (j == i) {
+          p++;
+        } else {
+          e++;
+        }
+      }
+    }
+  }
+
+  return std::to_string(e) + "e" + std::to_string(p) + "p";
+}
+
+int main() {
+  // const char *BOT_TOKEN = std::getenv("BOT_TOKEN");
+  // if (!BOT_TOKEN) {
+  //   std::cerr << "BOT_TOKEN missing from .env file!\n";
+  //   return 1;
+  // }
   dpp::cluster bot(BOT_TOKEN, dpp::i_default_intents | dpp::i_message_content);
   std::unordered_map<dpp::snowflake, bool> to_answer;
   char a[4];
+
   bot.on_log(dpp::utility::cout_logger());
 
   bot.on_slashcommand([&to_answer, &a](const dpp::slashcommand_t &event) {
     if (event.command.get_command_name() == "info") {
       event.reply("3manuel's Bot!");
     }
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(0, 9);
+
     if (event.command.get_command_name() == "game") {
-      for (int i = 0; i < 4; i++) {
-        char temp = rand() % 10;
-        while (std::find(a, a + i + 1, temp) != a + i + 1) {
-          temp = rand() % 10;
+
+      if (to_answer.empty()) {
+
+        for (int i = 0; i < 4; i++) {
+          char temp = dist(gen);
+          while (std::find(a, a + i + 1, temp) != a + i + 1) {
+            temp = dist(gen);
+          }
+          a[i] = temp;
         }
-        a[i] = temp;
+        std::string s = "";
+        for (char i : a)
+          s += std::to_string(i);
+
+        event.reply("A 4 digit number was generated\ne: means existing digit."
+                    "(but not in it's place).\np: means a digit is in it's "
+                    "place\ne and p are a total.");
+
+        to_answer[event.command.get_issuing_user().id] = true;
+
+      } else {
+        auto it = to_answer.begin();
+        auto id = it->first;
+        std::string s = "<@" + std::to_string(id) + "> is using the bot rn";
+        event.reply(s);
       }
-      std::string s = "";
-      for (char i : a)
-        s += std::to_string(i);
-      event.reply(s);
-      to_answer[event.command.get_issuing_user().id] = true;
     }
   });
 
   bot.on_message_create([&](const dpp::message_create_t &event) {
     if (to_answer.count(event.msg.author.id)) {
       std::string reply = event.msg.content;
-      if (reply.length() != 4) {
-        event.reply("must be 4 numbers");
+      if (reply == "stop") {
+        event.reply("Game over");
+        to_answer.erase(event.msg.author.id);
+      } else if (reply.length() != 4 || !isNumber(reply)) {
+        event.reply("must be 4 digit number :rage:");
+      } else if (!isNotRepeated(reply)) {
+        event.reply("must not repeat numbers :rage:");
       } else {
-        event.reply(reply);
+        std::string s = "";
+        for (int i = 0; i < 4; i++)
+          s += std::to_string(a[i]);
+        if (s == reply) {
+          to_answer.erase(event.msg.author.id);
+          event.reply("congrats you won");
+        } else {
+          std::string answer = strAnswer(reply, s);
+          event.reply(answer);
+        }
       }
       // to_answer.erase(event.msg.author.id);
     }
@@ -49,9 +139,9 @@ int main() {
     if (dpp::run_once<struct register_bot_commands>()) {
       bot.guild_command_create(
           dpp::slashcommand("game", "number guessing game", bot.me.id),
-          1368619088200601620);
+          GUILD_ID);
       bot.guild_command_create(dpp::slashcommand("info", "info", bot.me.id),
-                               1368619088200601620);
+                               GUILD_ID);
     }
   });
 
